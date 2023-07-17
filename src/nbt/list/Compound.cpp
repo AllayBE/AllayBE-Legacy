53663e6ca7c266892dc04b4dd08230c0ea6c831b
vp817 <118466306+vp817@users.noreply.github.com>
2023-07-17 19:38:19 +0300

    should be 8bits to not get the tag end

--- src/nbt/list/Compound.cpp
+++ src/nbt/list/Compound.cpp
@@ -35,7 +35,7 @@ bool Compound::DeserializeBE(BitStream *stream, bool withHeader)
 		Tag *tag = Nbt::GetTag(tagID);
 		tag->DeserializeBE(stream, false);
 		this->value.push_back(tag);
-	} while (stream->GetNumberOfBitsUsed() > 0);
+	} while (stream->GetNumberOfBitsUsed() > 8);
 	stream->IgnoreBits(8);
 	return true;
 }
@@ -64,7 +64,7 @@ bool Compound::DeserializeLE(BitStream *stream, bool withHeader)
 		Tag *tag = Nbt::GetTag(tagID);
 		tag->DeserializeLE(stream, false);
 		this->value.push_back(tag);
-	} while (stream->GetNumberOfBitsUsed() > 0);
+	} while (stream->GetNumberOfBitsUsed() > 8);
 	stream->IgnoreBits(8);
 	return true;
 }
@@ -93,7 +93,7 @@ bool Compound::DeserializeNET(BitStream *stream, bool withHeader)
 		Tag *tag = Nbt::GetTag(tagID);
 		tag->DeserializeNET(stream, false);
 		this->value.push_back(tag);
-	} while (stream->GetNumberOfBitsUsed() > 0);
+	} while (stream->GetNumberOfBitsUsed() > 8);
 	stream->IgnoreBits(8);
 	return true;
 }
